88e81c6523e254f48936512fd26a4363ed0502b0
Arcky97 <syntixis@gmail.com>
2024-12-03 22:28:39 +0100

    database handler improved

--- database/db.js
+++ database/db.js
@@ -15,206 +15,276 @@ async function initDatabase() {
       connectionLimit: 10,
       queueLimit: 0
     });
-
-    console.log('Connected to the database!');
-
-    //const editLevelSettingsTable = `ALTER TABLE LevelSettings CHANGE COLUMN announceLevelMessages TEXT AFTER announceDefaultMessage;`;
-    //await pool.query(editLevelSettingsTable);
-
-    //const editTable = `ALTER TABLE LevelSettings ADD multiplierReplace JSON DEFAULT '{ "category": true, "channel": true }'`
-    //const editTable = `ALTER TABLE GuildSettings ADD joinLeaveConfig JSON DEFAULT '{ "joins": true, "leaves": true }' AFTER joinLeaveLogging;`;
-    //const editTable = `ALTER TABLE GuildSettings DROP memberConfig`;
-    //const editTable = `ALTER TABLE GuildSettings MODIFY memberConfig JSON DEFAULT '{ "roles": { "adds": true, "removes": true }, "names": { "users": true, "globals": true, "nicks": true }, "avatars": { "globals": true, "servers": true }, "bans": { "adds": true, "removes": true }, "timeouts": { "adds": true, "removes": true } }'`
-    //const editTable = `ALTER TABLE LevelSettings CHANGE COLUMN multiplierReplace multiplierReplace JSON DEFAULT '{ "category": true, "channel": true }' AFTER categoryMultipliers`;
-    //const editTable = `ALTER TABLE LevelSettings MODIFY announceDefaultMessage JSON DEFAULT '{ "title": "{user global} leveled up!}", "description": "Congrats, {user global}, you leveled up to lv. {level}!", "color": "{user color}", "thumbnailUrl": "{user avatar}", "imageURL": null, "footer": { "text": "{server name}", "iconUrl": "{server icon}" }, "timeStamp": true }'`;
-    //const editTable = `ALTER TABLE LevelSettings MODIFY categoryMultipliers JSON DEFAULT '[]'`;
-    //const editTable = `ALTER TABLE LevelSettings CHANGE COLUMN xpSettings xpSettings JSON AFTER xpCooldown;`;
-    //const editTable = `ALTER TABLE LevelSystem MODIFY COLUMN xp BIGINT UNSIGNED DEFAULT 0`
-    //await pool.query(editTable);
-
-    //const dropBotRepliesTable = `DROP TABLE IF EXISTS BotReplies;`;
-    //await pool.query(dropBotRepliesTable);
-
-    //const dropGuildSettingsTable = `DROP TABLE IF EXISTS GuildSettings;`;
-    //await pool.query(dropGuildSettingsTable);
-
-    //const dropLevelXpRequirements = `DROP TABLE IF EXISTS LevelXpRequirements;`;
-    //await pool.query(dropLevelXpRequirements);
-
-    //const dropMemberLevelsTable = `DROP TABLE IF EXISTS LevelSystem;`;
-    //await pool.query(dropMemberLevelsTable);
-
-    //const droplevelSettingsTable = `DROP TABLE IF EXISTS LevelSettings;`;
-    //await pool.query(droplevelSettingsTable);
-
-    //onst dropEventsEmbedsTable = `DROP TABLE IF EXISTS EventEmbeds;`;
-    //await pool.query(dropEventsEmbedsTable);
-
-    //const dropGeneratedEmbedsTable = `DROP TABLE IF EXISTS GeneratedEmbeds;`;
-    //await pool.query(dropGeneratedEmbedsTable);
-
-    //const dropReactionRolesTable = `DROP TABLE IF EXISTS ReactionRoles;`;
-    //await pool.query(dropReactionRolesTable);
-
-    //const dropUserStatsTable = `DROP TABLE IF EXISTS UserStats;`;
-    //await pool.query(dropUserStatsTable);
-
-    const createGuildSettingsTable = `
-      CREATE TABLE IF NOT EXISTS GuildSettings (
-        guildId VARCHAR(100) NOT NULL PRIMARY KEY,
-        chattingChannel VARCHAR(100) DEFAULT NULL,
-        messageLogging VARCHAR(100) DEFAULT NULL,
-        memberLogging VARCHAR(100) DEFAULT NULL,
-        serverLogging VARCHAR(100) DEFAULT NULL,
-        voiceLogging VARCHAR(100) DEFAULT NULL,
-        joinLeaveLogging VARCHAR(100) DEFAULT NULL,
-        reportLogging VARCHAR(100) DEFAULT NULL,
-        ignoreLogging JSON DEFAULT '[]',
-        muteRole VARCHAR(100) DEFAULT NULL,
-        deletionDate TIMESTAMP NULL
-      );
-    `;
-
-    const createLevelSystemTable = `
-      CREATE TABLE IF NOT EXISTS LevelSystem (
-        guildId VARCHAR(100) NOT NULL,
-        memberId VARCHAR(100) NOT NULL,
-        level INT DEFAULT 0,
-        xp BIGINT DEFAULT 0,
-        color VARCHAR(10) DEFAULT '#f97316',
-        deletionDate TIMESTAMP NULL,
-        PRIMARY KEY (guildId, memberId)
-      );
-    `;
-
-    const createBotRepliesTable = `
-      CREATE TABLE IF NOT EXISTS BotReplies (
-        id VARCHAR(100) NOT NULL,
-        triggers JSON DEFAULT NULL,
-        responses JSON DEFAULT NULL,
-        PRIMARY KEY (id)
-      );
-    `;
-
-    const createLevelSettingsTable = `
-      CREATE TABLE IF NOT EXISTS LevelSettings (
-        guildId VARCHAR(100) NOT NULL PRIMARY KEY,
-        globalMultiplier INT DEFAULT 100,
-        levelRoles JSON DEFAULT '[]',
-        roleReplace BOOLEAN DEFAULT false,
-        announceChannel VARCHAR(100) DEFAULT 'not set',
-        announcePing BOOLEAN DEFAULT false,
-        announceDefaultMessage JSON DEFAULT '{ "title": "{user global} leveled up!}", "description": "Congrats, {user global}, you leveled up to lv. {level}!", "color": "{user color}", "thumbnailUrl": "{user avatar}", "imageURL": null, "footer": { "text": "{server name}", "iconUrl": "{server icon}" }, "timeStamp": true }',
-        announceLevelMessages JSON DEFAULT '[]',
-        roleMultipliers JSON DEFAULT '[]',
-        channelMultipliers JSON DEFAULT '[]',
-        categoryMultipliers JSON DEFAULT '[]',
-        multiplierReplace JSON DEFAULT '{ "category": true, "channel": true }',
-        blackListRoles JSON DEFAULT '[]',
-        blackListChannels JSON DEFAULT '[]',
-        blackListCategories JSON DEFAULT '[]',
-        xpCooldown INT DEFAULT 30,
-        xpSettings JSON DEFAULT '{ "step": 40, "min": 15, "max": 25 }',
-        clearOnLeave BOOLEAN DEFAULT false,
-        voiceEnable BOOLEAN DEFAULT false,
-        voiceMultiplier INT DEFAULT 1,
-        voiceCooldown INT DEFAULT 2,
-        deletionDate TIMESTAMP NULL
-      );
-    `;
-
-    const createLevelXpRequirements = `
-      CREATE TABLE IF NOT EXISTS LevelXpRequirements (
-        level INT NOT NULL PRIMARY KEY,
-        requiredXp INT NOT NULL
-      )
-    `;
-
-    const createEventEmbedsTable = `
-      CREATE TABLE IF NOT EXISTS EventEmbeds (
-        guildId VARCHAR(100) NOT NULL,
-        channelId VARCHAR(100) NOT NULL,
-        type VARCHAR(50) NOT NULL,
-        message VARCHAR(2000) DEFAULT NULL,
-        color VARCHAR(10) DEFAULT NULL,
-        author VARCHAR(256) DEFAULT NULL,
-        authorUrl VARCHAR(512) DEFAULT NULL,
-        authorIconUrl VARCHAR(512) DEFAULT NULL,
-        title VARCHAR(256) DEFAULT NULL,
-        titleUrl VARCHAR(512) DEFAULT NULL,
-        description VARCHAR(2048) NOT NULL,
-        fields JSON DEFAULT NULL,
-        imageUrl VARCHAR(512) DEFAULT NULL,
-        thumbnailUrl VARCHAR(512) DEFAULT NULL,
-        footer VARCHAR(2048) DEFAULT NULL,
-        footerIconUrl VARCHAR(512) DEFAULT NULL,
-        timeStamp BOOLEAN DEFAULT true,
-        deletionDate TIMESTAMP NULL,
-        PRIMARY KEY (guildId, channelId, type)
-      )
-    `;
-
-    const createGeneratedEmbedsTable = `
-      CREATE TABLE IF NOT EXISTS GeneratedEmbeds (
-        guildId VARCHAR(100) NOT NULL,
-        channelId VARCHAR(100) NOT NULL,
-        messageId VARCHAR(100) NOT NULL,
-        message VARCHAR(2000) DEFAULT NULL,
-        color VARCHAR(10) DEFAULT NULL,
-        author VARCHAR(256) DEFAULT NULL,
-        authorUrl VARCHAR(512) DEFAULT NULL,
-        authorIconUrl VARCHAR(512) DEFAULT NULL,
-        title VARCHAR(256) NOT NULL,
-        titleUrl VARCHAR(512) DEFAULT NULL,
-        description VARCHAR(2048) NOT NULL,
-        fields JSON DEFAULT NULL,
-        imageUrl VARCHAR(512) DEFAULT NULL,
-        thumbnailUrl VARCHAR(512) DEFAULT NULL,
-        footer VARCHAR(2048) DEFAULT NULL,
-        footerIconUrl VARCHAR(512) DEFAULT NULL,
-        timeStamp BOOLEAN DEFAULT true,
-        deletionDate TIMESTAMP NULL,
-        PRIMARY KEY (guildId, channelId, messageId)
-      )
-    `;
-
-    const createReactionRolesTable = `
-      CREATE TABLE IF NOT EXISTS ReactionRoles (
-        guildId VARCHAR(100) NOT NULL,
-        channelId VARCHAR(100) NOT NULL,
-        messageId VARCHAR(100) NOT NULL,
-        emojiRolePairs JSON DEFAULT NULL,
-        maxRoles INT DEFAULT 0,
-        maxReactions INT DEFAULT 0,
-        type VARCHAR(100) DEFAULT 'normal',
-        deletionDate TIMESTAMP NULL,
-        PRIMARY KEY (guildId, channelId, messageId)
-      )
-    `;
-
-    const createUserStatsTable = `
-      CREATE TABLE IF NOT EXISTS UserStats (
-        guildId VARCHAR(100) NOT NULL,
-        memberId VARCHAR(100) NOT NULL,
-        attempts JSON DEFAULT '{ "slap": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }, "kick": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }, "ban": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }, "mute": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }}',
-        PRIMARY KEY (guildId, memberId)
-      )
-    `;
     
-    await pool.query(createGuildSettingsTable);
-    await pool.query(createLevelSystemTable);
-    await pool.query(createBotRepliesTable);
-    await pool.query(createLevelSettingsTable);
-    //await pool.query(createLevelXpRequirements);
-    await pool.query(createEventEmbedsTable);
-    await pool.query(createGeneratedEmbedsTable);
-    await pool.query(createReactionRolesTable);
-    await pool.query(createUserStatsTable);
-    console.log('Tables Created!');
+    console.log('Connected to the database!');
+    console.log('-----------------------------------');
+    console.log('Checking database structure...');
+
+    const desiredTables = {
+      GuildSettings: `
+        CREATE TABLE IF NOT EXISTS GuildSettings (
+          guildId VARCHAR(100) NOT NULL PRIMARY KEY,
+          chattingChannel VARCHAR(100) DEFAULT NULL,
+          messageLogging VARCHAR(100) DEFAULT NULL,
+          memberLogging VARCHAR(100) DEFAULT NULL,
+          serverLogging VARCHAR(100) DEFAULT NULL,
+          voiceLogging VARCHAR(100) DEFAULT NULL,
+          joinLeaveLogging VARCHAR(100) DEFAULT NULL,
+          reportLogging VARCHAR(100) DEFAULT NULL,
+          ignoreLogging JSON DEFAULT '[]',
+          muteRole VARCHAR(100) DEFAULT NULL,
+          deletionDate TIMESTAMP NULL
+        );
+      `,
+      LevelSystem: `
+        CREATE TABLE IF NOT EXISTS LevelSystem (
+          guildId VARCHAR(100) NOT NULL,
+          memberId VARCHAR(100) NOT NULL,
+          level INT DEFAULT 0,
+          xp BIGINT DEFAULT 0,
+          color VARCHAR(10) DEFAULT '#f97316',
+          deletionDate TIMESTAMP NULL,
+          PRIMARY KEY (guildId, memberId)
+        );
+      `,
+      BotReplies: `
+        CREATE TABLE IF NOT EXISTS BotReplies (
+          id VARCHAR(100) NOT NULL,
+          triggers JSON DEFAULT NULL,
+          responses JSON DEFAULT NULL,
+          PRIMARY KEY (id)
+        );
+      `,
+      LevelSettings: `
+        CREATE TABLE IF NOT EXISTS LevelSettings (
+          guildId VARCHAR(100) NOT NULL PRIMARY KEY,
+          globalMultiplier INT DEFAULT 100,
+          levelRoles JSON DEFAULT '[]',
+          roleReplace BOOLEAN DEFAULT false,
+          announceChannel VARCHAR(100) DEFAULT 'not set',
+          announcePing BOOLEAN DEFAULT false,
+          announceDefaultMessage JSON DEFAULT '{ "title": "{user global} leveled up!}", "description": "Congrats, {user global}, you leveled up to lv. {level}!", "color": "{user color}", "thumbnailUrl": "{user avatar}", "imageURL": null, "footer": { "text": "{server name}", "iconUrl": "{server icon}" }, "timeStamp": true }',
+          announceLevelMessages JSON DEFAULT '[]',
+          roleMultipliers JSON DEFAULT '[]',
+          channelMultipliers JSON DEFAULT '[]',
+          categoryMultipliers JSON DEFAULT '[]',
+          multiplierReplace JSON DEFAULT '{ "category": true, "channel": true }',
+          blackListRoles JSON DEFAULT '[]',
+          blackListChannels JSON DEFAULT '[]',
+          blackListCategories JSON DEFAULT '[]',
+          xpCooldown INT DEFAULT 30,
+          xpSettings JSON DEFAULT '{ "step": 40, "min": 15, "max": 25 }',
+          clearOnLeave BOOLEAN DEFAULT false,
+          voiceEnable BOOLEAN DEFAULT false,
+          voiceMultiplier INT DEFAULT 1,
+          voiceCooldown INT DEFAULT 2,
+          deletionDate TIMESTAMP NULL
+        );
+      `,
+      EventEmbeds: `
+        CREATE TABLE IF NOT EXISTS EventEmbeds (
+          guildId VARCHAR(100) NOT NULL,
+          channelId VARCHAR(100) NOT NULL,
+          type VARCHAR(50) NOT NULL,
+          message VARCHAR(2000) DEFAULT NULL,
+          color VARCHAR(10) DEFAULT NULL,
+          author VARCHAR(256) DEFAULT NULL,
+          authorUrl VARCHAR(512) DEFAULT NULL,
+          authorIconUrl VARCHAR(512) DEFAULT NULL,
+          title VARCHAR(256) DEFAULT NULL,
+          titleUrl VARCHAR(512) DEFAULT NULL,
+          description VARCHAR(2048) NOT NULL,
+          fields JSON DEFAULT NULL,
+          imageUrl VARCHAR(512) DEFAULT NULL,
+          thumbnailUrl VARCHAR(512) DEFAULT NULL,
+          footer VARCHAR(2048) DEFAULT NULL,
+          footerIconUrl VARCHAR(512) DEFAULT NULL,
+          timeStamp BOOLEAN DEFAULT true,
+          deletionDate TIMESTAMP NULL,
+          PRIMARY KEY (guildId, channelId, type)
+        )
+      `,
+      GeneratedEmbeds: `
+        CREATE TABLE IF NOT EXISTS GeneratedEmbeds (
+          guildId VARCHAR(100) NOT NULL,
+          channelId VARCHAR(100) NOT NULL,
+          messageId VARCHAR(100) NOT NULL,
+          message VARCHAR(2000) DEFAULT NULL,
+          color VARCHAR(10) DEFAULT NULL,
+          author VARCHAR(256) DEFAULT NULL,
+          authorUrl VARCHAR(512) DEFAULT NULL,
+          authorIconUrl VARCHAR(512) DEFAULT NULL,
+          title VARCHAR(256) NOT NULL,
+          titleUrl VARCHAR(512) DEFAULT NULL,
+          description VARCHAR(2048) NOT NULL,
+          fields JSON DEFAULT NULL,
+          imageUrl VARCHAR(512) DEFAULT NULL,
+          thumbnailUrl VARCHAR(512) DEFAULT NULL,
+          footer VARCHAR(2048) DEFAULT NULL,
+          footerIconUrl VARCHAR(512) DEFAULT NULL,
+          timeStamp BOOLEAN DEFAULT true,
+          deletionDate TIMESTAMP NULL,
+          PRIMARY KEY (guildId, channelId, messageId)
+        )
+      `,
+      ReactionRoles: `
+        CREATE TABLE IF NOT EXISTS ReactionRoles (
+          guildId VARCHAR(100) NOT NULL,
+          channelId VARCHAR(100) NOT NULL,
+          messageId VARCHAR(100) NOT NULL,
+          emojiRolePairs JSON DEFAULT NULL,
+          maxRoles INT DEFAULT 0,
+          maxReactions INT DEFAULT 0,
+          type VARCHAR(100) DEFAULT 'normal',
+          deletionDate TIMESTAMP NULL,
+          PRIMARY KEY (guildId, channelId, messageId)
+        )
+      `,
+      UserStats: `
+        CREATE TABLE IF NOT EXISTS UserStats (
+          guildId VARCHAR(100) NOT NULL,
+          memberId VARCHAR(100) NOT NULL,
+          attempts JSON DEFAULT '{ "slap": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }, "kick": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }, "ban": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }, "mute": { "client": 0, "owner": 0, "self": 0, "admins": {}, "members": {}, "bots": {} }}',
+          PRIMARY KEY (guildId, memberId)
+        )
+      `,
+      TestTable: `
+        CREATE TABLE IF NOT EXISTS TestTable (
+          id VARCHAR(100) NOT NULL PRIMARY KEY,
+          name VARCHAR(100) NOT NULL
+        )
+      `
+    };
+    
+    const [currentTables] = await pool.query(
+      `SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = ?`,
+      [process.env.DB_NAME]
+    );
+    
+    const currentTableNames = currentTables.map(row => row.TABLE_NAME);
+
+    for (const [tableName, createQuery] of Object.entries(desiredTables)) {
+      if (!currentTableNames.includes(tableName)) {
+        console.log(`New table "${tableName}" created.`);
+        await pool.query(createQuery);
+      } else {
+        //console.log(`Table "${tableName}" already exists. Checking for updates...`);
+        // Voeg hier logica toe om bestaande tabellen te controleren op wijzigingen
+        await checkTableUpdates(tableName);
+      }
+    }
+
+    console.log('Database structure check complete.');
   } catch (err) {
     console.error('Error initializing the database:', err);
   }
   console.log('-----------------------------------');
+}
+
+async function checkTableUpdates(tableName) {
+  const [columns] = await pool.query(
+    `SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT 
+    FROM information_schema.columns 
+    WHERE table_name = ? AND table_schema = ?`,
+    [tableName, process.env.DB_NAME]
+  );
+
+  const columnMap = columns.reduce((map, col) => {
+    map[col.COLUMN_NAME] = {
+      type: col.COLUMN_TYPE,
+      nullable: col.IS_NULLABLE === 'YES',
+      default: col.COLUMN_DEFAULT
+    };
+    return map;
+  }, {});
+  console.log(`No changes made to table "${tableName}".`);
+}
+
+async function getExistingColumns(tableName) {
+  try {
+    const [columns] = await pool.query(
+      `SELECT COLUMN_NAME FROM information_schema.columns WHERE table_name = ? AND table_schema = ?`,
+      [tableName, process.env.DB_NAME]
+    );
+
+    return columns.map(col => col.COLUMN_NAME);
+  } catch (error) {
+    console.error(`Error getting Existing Table "${tableName}":`, error);
+  }
+}
+
+async function addColumn(tableName, columnName, columnDefinition) {
+  try {
+    const existingColumns = await getExistingColumns(tableName);
+
+    if (!existingColumns.includes(columnName)) {
+      await pool.query(
+      `ALTER TABLE ${tableName} ADD COLUMN ${columnName} ${columnDefinition}`
+      );
+      console.log(`Column "${columnName}" added to Table "${tableName}".`);
+    } else {
+      console.log(`Column "${columnName}" already exists in Table "${tableName}"`);
+    }
+  } catch(error) {
+    console.error(`Error when Adding Column "${columnName}" in Table "${tableName}":`, error);
+  }
+}
+
+async function modifyColumn(tableName, columnName, newDefinition) {
+  try {
+    const existingColumns = await getExistingColumns(tableName);
+
+    if (!existingColumns.includes(columnName)) {
+      await pool.query(
+        `ALTER TABLE ${tableName} MODIFY COLUMN ${columnName} ${newDefinition}`
+      );
+      console.log(`Column "${columnName}" Modified in Table "${tableName}".`);
+    } else {
+      console.log(`Column "${columnName}" does not Exist in Table "${tableName}".`);
+    }
+  } catch (error) {
+    console.error(`Error when Modyfying Column "${columnName}" in Table "${tableName}":`, error);
+  }
+}
+
+async function removeColumn(tableName, columnName) {
+  try {
+    const existingColumns = await getExistingColumns(tableName);
+
+    if (!existingColumns.includes(columnName)) {
+      await pool.query(
+        `ALTER TABLE ${tableName} DROP COLUMN ${columnName}`
+      );
+      console.log(`Column "${columnName}" Removed from Table "${tableName}".`);
+    } else {
+      console.log(`Column "${tableName}" does not Exist in Table "${tableName}".`);
+    }
+  } catch (error) {
+    console.log(`Error when Removing Column "${columnName}" from Table "${tableName}":`, error);
+  }
+}
+
+async function resetTable(tableName, createQuery) {
+  try {
+    await pool.query(`DROP TABLE IF EXISTS ${tableName}`);
+    console.log(`Table "${tableName}" has been Resetted.`);
+
+    if (createQuery) {
+      await pool.query(createQuery);
+      console.log(`Table "${tableName}" Recreated.`);
+    }
+  } catch (error) {
+    console.error(`Error Resetting Table "${tableName}":`, error);
+  }
+}
+
+async function deleteTable(tableName) {
+  try {
+    await pool.query(`DROP TABLE IF EXISTS ${tableName}`);
+    console.log(`Table "${tableName}" Removed.`);
+  } catch (error) {
+    console.error(`Error Removing Table "${tableName}":`, error);
+  }
 }
 
 // Function to execute queries
