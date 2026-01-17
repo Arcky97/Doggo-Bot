415c061478146279056d8df0c195b45fbab8cf37
Arcky97 <syntixis@gmail.com>
2026-01-17 22:43:31 +0100

    small fix

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -2,7 +2,7 @@ import botActivityService from "../../services/botActivityService.js";
 
 export default () => {
   console.log(
-    `'${client.user.username}' is ready! v2025-12-14\n` + 
+    `'${client.user.username}' is ready! v2026-01-17\n` + 
     '-----------------------------------'
   );
   botActivityService('Bot Started');
